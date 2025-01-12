30c1131ad40d7d9ce2a3932d353e09e8c0ca520e
chrisbolosan <cbolosan@gmail.com>
2025-01-11 21:43:25 -0600

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'], // Extensions for your pages
+  trailingSlash: false,
 };
 
 export default nextConfig;
