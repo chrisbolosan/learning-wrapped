9369b6037827eee0714d876d37fdc7326e649170
chrisbolosan <cbolosan@gmail.com>
2025-01-13 18:06:31 -0600

    reroute default url to spaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+'use client';
 
-const page = () => {
-  return <div>Home</div>;
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const Page: React.FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/spaces');
+  }, [router]);
+
+  return null;
 };
 
-export default page;
+export default Page;
