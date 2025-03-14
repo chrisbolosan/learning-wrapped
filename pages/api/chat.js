85efa93320e6eaabf4df5dac02800ce4cb071172
chrisbolosan <cbolosan@gmail.com>
2025-03-13 22:24:19 -0500

    update  AI Model

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash-002' });
 
     const chat = model.startChat({
       history: [
